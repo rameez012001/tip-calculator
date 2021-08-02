1760f026128647b96e760d76bb9d5345d2279bd0
Rameez Hameed <rameez012001@gmail.com>
2021-08-02 23:45:17 +0530

    desktop completed

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 function Percent(x){
     let bll = document.getElementById('TBllamnt');
     let Prsn = document.getElementById('PeNr');
-    let amt = x*100/bll.value;
+    let amt = (x/100)*bll.value;
     let amt1 = amt/Prsn.value;
-    let amt2 = (parseInt(bll.value)+parseInt(amt))/Prsn.value;
-    document.getElementById('TpAt').value=amt1;
-    document.getElementById('BlAt').value=amt2;
+    let amt2 = (parseInt(bll.value)+parseFloat(amt))/Prsn.value;
+    document.getElementById('TpAt').innerHTML="$"+amt1.toFixed(2);
+    document.getElementById('BlAt').innerHTML="$"+amt2.toFixed(2);
     if(Prsn.value==""||Prsn.value==null){
         Prsn.style.border="1px solid red";
-        document.getElementById('TpAt').value="";
-        document.getElementById('BlAt').value="";
-        document.getElementById('ErrMsg').style.display='block';
+        document.getElementById('TpAt').innerHTML="$0.00";
+        document.getElementById('BlAt').innerHTML="$0.00";
+        document.getElementById('ErrMsg').style.display='inline';
     }else{
         Prsn.style.border="none";
         document.getElementById('ErrMsg').style.display='none';
@@ -18,5 +18,9 @@ function Percent(x){
 }
 function Reset(){
     const Inputs = document.querySelectorAll(".Inpts");
-    Inputs.forEach((x)=>{x.value=""})
+    Inputs.forEach((x)=>{x.value=""});
+    const Amnts = document.querySelectorAll(".Amnts1");
+    Amnts.forEach((y)=>{y.innerHTML="$0.00"});    
+    document.getElementById('PeNr').style.border="none";
+    document.getElementById('ErrMsg').style.display='none';
 }
